88f274de8f7b981601d76f7f3c008ef43a19877d
Hubert Sokołowski <hubert.sokool@outlook.com>
2019-06-21 00:14:25 +0200

    add CRUD operations

--- WetherApp/Controllers/HomeController.cs
+++ WetherApp/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -83,6 +84,72 @@ namespace WetherApp.Controllers
             db.SaveChanges();
 
             return RedirectToAction("Contact");
+        }
+        public ActionResult Delete(int id)
+        {
+            using (DB db = new DB())
+            {
+                CityDto _city = new CityDto() { id = id };
+                db.Cities.Attach(_city);
+                db.Cities.Remove(_city);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            
+        }
+
+        public ActionResult Details(int id)
+        {
+            CityDto _city = new CityDto();
+            using (DB db = new DB())
+            {
+                
+                db.Cities.Attach(_city);
+                _city = db.Cities.Find(id);
+            }
+            return View(_city);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            using (DB db = new DB())
+            {
+                CityDto _city = db.Cities.Where(s => s.id == id).FirstOrDefault();
+                return View(_city);
+            }
+                
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(CityDto c)
+        {
+            using (DB db = new DB())
+            {
+                db.Entry(c).State = EntityState.Modified;
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (System.Data.Entity.Validation.DbEntityValidationException dbEx)
+                {
+                    Exception raise = dbEx;
+                    foreach (var validationErrors in dbEx.EntityValidationErrors)
+                    {
+                        foreach (var validationError in validationErrors.ValidationErrors)
+                        {
+                            string message = string.Format("{0}:{1}",
+                                validationErrors.Entry.Entity.ToString(),
+                                validationError.ErrorMessage);
+                            // raise a new exception nesting
+                            // the current instance as InnerException
+                            raise = new InvalidOperationException(message, raise);
+                        }
+                    }
+                    throw raise;
+                }
+                return RedirectToAction("Index");
+            }
+            
         }
     }
 }
---
+++ WetherApp/Migrations/201906202156394_setdb.cs
@@ -0,0 +1,16 @@
+namespace WetherApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class setdb : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
